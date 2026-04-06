7d4e278d2ba602256cf840e9e71eaeb9965f7412
Alfred <113814260+AlfredLiu13@users.noreply.github.com>
2026-04-06 13:04:48 -0400

    Added SLURM args as comments

--- classification/classification.py
+++ classification/classification.py
@@ -4,6 +4,25 @@ from main import run_classification
 import logging
 import argparse
 
+"""
+#!/bin/bash
+#SBATCH -J classification
+#SBATCH -p RM-shared
+#SBATCH -N 1
+#SBATCH -n 2
+#SBATCH -t 12:00:00
+#SBATCH --mem-per-cpu=2000M 
+#SBATCH -o logs/classification_%j.out 
+#SBATCH -e logs/classification_%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=alfredl@andrew.cmu.edu
+
+module load anaconda3
+conda activate my_bio_env
+
+./classify_ocr.py --config /path/to/config.yaml
+"""
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s"
